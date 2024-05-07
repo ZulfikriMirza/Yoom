671b479c59463111362f28de3ed0cd9f23636764
unknown <zulfikrimirza@yahoo.com>
2024-05-07 14:20:07 +0700

    1:38:01

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,9 +1,18 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 
-const HomeCard = () => {
+interface HomeCardProps {
+    className: string,
+    img: string,
+    title: string,
+    description: string,
+    handleClick: () =>void;
+}
+
+const HomeCard = ({ className, img, title, description, handleClick }: HomeCardProps) => {
     return (
-        <div className='bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer'>
+        <div className={cn('px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer' onClick={() => {}}>
             <div className="flex-center glassmorphism size-12 rounded-[10px]">
                 <Image
                   src='/icons/add-meeting.svg'
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,16 +1,50 @@
 'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import HomeCard from "./HomeCard"
+import { useRouter } from 'next/navigation';
 
 const MeetingTypeList = () => {
+  const router = useRouter();
+  const [meetingState, setmeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
 
     return (
         <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
+            <HomeCard 
+              img="/icons/add-meeting.svg"
+              title="New Meeting"
+              description="start an instant meeting"
+              handleClick={() => setMeetingState('isJoiningMeeting')}
+              className="bg-orange-1"
+
+            />
+
+            
+
+            <HomeCard 
+                          img="/icons/schedule.svg"
+                          title="Schedule Meeting"
+                          description="Plan your meeting"
+                          handleClick={() => setMeetingState('isScheduleMeeting')}
+                     className="bg-blue-1"
+            />
+            <HomeCard 
+              img="/icons/recordings.svg"
+              title="View Recordings"
+              description="Check out your recording"
+              className = "bg-purple-1"
+              handleClick={() => router.push('/recordings')}
+          
+ />
+            <HomeCard 
+               img="/icons/add-meeting.svg"
+               title="New Meeting"
+               description="start an instant meeting"
+               className="bg-yellow-1"
+               handleClick={() => router.push('/recordings')}
+                     
+            
+            />
         </section>
   )
 }
